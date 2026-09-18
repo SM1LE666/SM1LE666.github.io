58f14dea6665eb09315f9ad04073cc7f7022f333
Kirill Krivyonok <krivenokk082007@gmail.com>
2026-09-18 13:05:05 +0300

    .

--- app/features/rendering.js
+++ app/features/rendering.js
@@ -306,7 +306,7 @@
             onclick="window.open('https://www.faceit.com/${profileLang}/players/${playerData.nickname}', '_blank')"
             style="cursor: pointer; width: 45px; height: 45px; margin-top: 5px; border-radius: 8px; border: 2px solid var(--primary-color); transition: transform 0.3s, box-shadow 0.3s; object-fit: contain;"
             onmouseover="this.style.transform='scale(1.1)'; this.style.boxShadow='0 0 10px var(--primary-color)';"
-            onmouseout="this.style.transform='scale(1)';"
+            onmouseout="this.style.transform='scale(1)'; this.style.boxShadow='none';"
           />
         </div>
       </div>
