584390347eabf959c6234392905e43b4637bcd22
Kirill Krivyonok <krivenokk082007@gmail.com>
2026-09-22 20:36:06 +0300

    .

--- app/state.js
+++ app/state.js
@@ -1,4 +1,18 @@
 (function () {
+  class Config {
+    constructor() {
+      this.loaded = false;
+      this.apiBaseUrl = "https://api.faceit.com";
+    }
+
+    async loadConfig() {
+      this.loaded = true;
+      return true;
+    }
+  }
+
+  window.Config = new Config();
+
   window.AppState = {
     playerStats: null,
     isInitialized: false,
